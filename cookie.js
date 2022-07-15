67016f3218e94df2cc2bd18625f50df24f9ce4eb
Krithivasan1816 <90230440+Krithivasan1816@users.noreply.github.com>
2022-07-15 11:54:48 +0530

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,5 +1,5 @@
 function setcookie() {
-  document.cookie = "email=abc@gmail.com";
+  document.cookie = "email=abc@gmail.com;expires=Thu, 18 Dec 2022 12:00:00 UTC";
 }
 function getcookie() {
   let x = document.cookie;
